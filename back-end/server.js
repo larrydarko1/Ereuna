42e929e18364f5bece3f8ffb998eb47fc8da569f
Larry Darko <l.mazzola96@tutanota.com>
2025-01-10 18:14:37 +0100

    fuck

--- back-end/server.js
+++ back-end/server.js
@@ -35,7 +35,7 @@ const allowedOrigins = [
 
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
-  max: 50000, // Limit each IP to 1000 requests per window
+  max: 50000, // Limit each IP to 50000 requests per window
   standardHeaders: true,
   legacyHeaders: false,
   message: {
