b2e4f82502c53dbe844e9e96166ecf89afaf73b1
Larry Darko <l.mazzola96@tutanota.com>
2025-05-28 15:23:33 +0200

    mobile docs

--- src/components/Docs/WelcomeDoc.vue
+++ src/components/Docs/WelcomeDoc.vue
@@ -16,28 +16,35 @@
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: center;
-    min-height: 100vh;
     text-align: center;
-    position: absolute;
-    top: 40%;
-    left: 70%;
-    transform: translate(-50%, -50%);
+    width: 100%;
 }
 
-.center-container h1{
+.center-container h1 {
     font-size: 2rem;
     color: $accent1;
     margin-bottom: 1rem;
 }
 
-.center-container p{
+.center-container p {
     font-size: 1.2rem;
     color: $text2;
     margin-bottom: 1rem;
+    overflow-wrap: anywhere;
 }
+
 .welcome-gif {
-    width: 200px;
+    width: 100%;
+    max-width: 200px;
     height: auto;
+    margin-bottom: 1.5rem;
 }
+
+//mobile version
+@media (max-width: 768px) {
+   .center-container {
+    margin-left: 0;
+}
+}
+
 </style>
