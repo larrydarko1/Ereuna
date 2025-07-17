8da81bf683c052c1e89931f134ed78197f2a71ee
Larry Darko <l.mazzola96@tutanota.com>
2025-07-17 16:48:59 +0200

    quasi

--- src/views/Charts.vue
+++ src/views/Charts.vue
@@ -113,7 +113,7 @@
         <div id="chart-container">
           <div id="legend"></div>
           <div id="legend2">
-            <div style="display: flex; gap: 5px;">
+            <div style="display: flex; gap: 5px; margin-bottom: 5px;">
                 <button
     v-for="type in chartTypes"
     :key="type.value"
@@ -124,10 +124,15 @@
     {{ type.shortLabel }}
   </button>
             </div>
-            <button class="navbtng" v-b-tooltip.hover title="Change Chart type" @click="toggleChartType"
+            <div style="display: flex; gap: 5px;">
+              <button class="navbt" v-b-tooltip.hover title="Change Chart type" @click="toggleChartType"
               aria-label="Toggle chart type">
-              {{ isBarChart ? 'Candlestick' : 'Bar' }}
+              {{ isBarChart ? 'C' : 'B' }}
             </button>
+            <button class="navbt">
+             <svg width="5" height="5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M2 15.6157C2 16.463 2.68179 17.1448 4.04537 18.5083L5.49167 19.9546C6.85525 21.3182 7.53704 22 8.38426 22C9.23148 22 9.91327 21.3182 11.2769 19.9546L19.9546 11.2769C21.3182 9.91327 22 9.23148 22 8.38426C22 7.53704 21.3182 6.85525 19.9546 5.49167L18.5083 4.04537C17.1448 2.68179 16.463 2 15.6157 2C14.8623 2 14.2396 2.53926 13.1519 3.61778C13.1817 3.63981 13.2103 3.66433 13.2373 3.69135L14.6515 5.10556C14.9444 5.39846 14.9444 5.87333 14.6515 6.16622C14.3586 6.45912 13.8837 6.45912 13.5908 6.16622L12.1766 4.75201C12.1494 4.7248 12.1247 4.69601 12.1026 4.66595L11.0299 5.73861C11.06 5.76077 11.0888 5.78545 11.116 5.81267L13.2373 7.93399C13.5302 8.22688 13.5302 8.70176 13.2373 8.99465C12.9444 9.28754 12.4695 9.28754 12.1766 8.99465L10.0553 6.87333C10.0281 6.84612 10.0034 6.81733 9.98125 6.78726L8.90859 7.85993C8.93865 7.88209 8.96744 7.90678 8.99465 7.93399L10.4089 9.3482C10.7018 9.6411 10.7018 10.116 10.4089 10.4089C10.116 10.7018 9.6411 10.7018 9.3482 10.4089L7.93399 8.99465C7.90678 8.96744 7.88209 8.93865 7.85993 8.90859L6.78727 9.98125C6.81733 10.0034 6.84612 10.0281 6.87333 10.0553L8.99465 12.1766C9.28754 12.4695 9.28754 12.9444 8.99465 13.2373C8.70176 13.5302 8.22688 13.5302 7.93399 13.2373L5.81267 11.116C5.78545 11.0888 5.76077 11.06 5.73861 11.0299L4.66595 12.1026C4.69601 12.1247 4.7248 12.1494 4.75201 12.1766L6.16622 13.5908C6.45912 13.8837 6.45912 14.3586 6.16622 14.6515C5.87333 14.9444 5.39846 14.9444 5.10556 14.6515L3.69135 13.2373C3.66433 13.2103 3.63981 13.1817 3.61778 13.1519C2.53926 14.2396 2 14.8623 2 15.6157Z" fill="#ffffff"></path> </g></svg>
+            </button>
+            </div>
           </div>
           <div id="legend3"></div>
           <div id="legend4">
@@ -1206,7 +1211,7 @@ async function fetchChartData() {
 let chartView = ref('Daily Chart');
 
 const isLoading = ref(true)
-const charttype = ref('Candlestick')
+const charttype = ref('C')
 const isBarChart = ref(false);
 
 const chartTypes = [
@@ -1286,6 +1291,10 @@ function resizeChart() {
     const { width, height } = getChartDimensions();
     chartInstance.value.applyOptions({ width, height });
   }
+}
+
+    function toggleChartType() {
+  isBarChart.value = !isBarChart.value;
 }
 
 // mounts chart (including volume)
@@ -1379,7 +1388,6 @@ onMounted(async () => {
       barSeries.setData(changes);
       updateLastRecordedValue(changes);
       Histogram.setData(typeObj.volume());
-      // MA series
       MaSeries1.setData(typeObj.ma[0]());
       MaSeries2.setData(typeObj.ma[1]());
       MaSeries3.setData(typeObj.ma[2]());
@@ -1496,97 +1504,6 @@ watch([priceTarget, showPriceTarget], ([newTarget, visible]) => {
   }
 });
 
-    watch(data, (newData) => {
-      const changes = calculateChanges(newData);
-      if (!useAlternateData) {
-        barSeries.setData(changes);
-        updateLastRecordedValue(changes);
-      }
-    });
-
-    watch(data7, (newData7) => {
-      const changes = calculateChanges(newData7);
-      if (useAlternateData) {
-        barSeries.setData(changes);
-        updateLastRecordedValue(changes);
-      }
-    });
-
-    watch(data2, (newData2) => {
-      if (!useAlternateData) {
-        Histogram.setData(newData2);
-      }
-    });
-
-    watch(data8, (newData8) => {
-      if (useAlternateData) {
-        Histogram.setData(newData8);
-      }
-    });
-
-    watch(data3, (newData3) => {
-      if (newData3 === null) {
-        MaSeries1.setData([]); // Clear the series data
-      } else if (!useAlternateData) {
-        MaSeries1.setData(newData3);
-      }
-    });
-
-    watch(data4, (newData4) => {
-      if (newData4 === null) {
-        MaSeries2.setData([]); // Clear the series data
-      } else if (!useAlternateData) {
-        MaSeries2.setData(newData4);
-      }
-    });
-
-    watch(data5, (newData5) => {
-      if (newData5 === null) {
-        MaSeries3.setData([]); // Clear the series data
-      } else if (!useAlternateData) {
-        MaSeries3.setData(newData5);
-      }
-    });
-
-    watch(data6, (newData6) => {
-      if (newData6 === null) {
-        MaSeries4.setData([]); // Clear the series data
-      } else if (!useAlternateData) {
-        MaSeries4.setData(newData6);
-      }
-    });
-
-    watch(data9, (newData9) => {
-      if (newData9 === null) {
-        MaSeries1.setData([]); // Clear the series data
-      } else if (useAlternateData) {
-        MaSeries1.setData(newData9);
-      }
-    });
-
-    watch(data10, (newData10) => {
-      if (newData10 === null) {
-        MaSeries2.setData([]); // Clear the series data
-      } else if (useAlternateData) {
-        MaSeries2.setData(newData10);
-      }
-    });
-
-    watch(data11, (newData11) => {
-      if (newData11 === null) {
-        MaSeries3.setData([]); // Clear the series data
-      } else if (useAlternateData) {
-        MaSeries3.setData(newData11);
-      }
-    });
-
-    watch(data12, (newData12) => {
-      if (newData12 === null) {
-        MaSeries4.setData([]); // Clear the series data
-      } else if (useAlternateData) {
-        MaSeries4.setData(newData12);
-      }
-    });
 
     watch(data2, (newData2) => {
       const relativeVolumeData = newData2.map((dataPoint, index) => {
